c5b90f968526a2597113d837421515f73160f867
Guilherme Lima <76075799+Guilherme-mtk@users.noreply.github.com>
2021-03-22 08:06:49 -0300

    alterando a font

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function typewriter(elemento) {
+  const textoArray = elemento.innerHTML.slipt('');
+  elemento.innerHTML = '';
+  textoArray.forEach((letra,i)=> {
+    setTimeout()
+  });
+}
+
+
+const titulo = document.querySelector('h1');
+typewriter(titulo);
+
