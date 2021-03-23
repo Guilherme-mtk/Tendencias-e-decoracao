0433899c1389e722eb5f091f8c627cff7b39e2d9
Guilherme Lima <76075799+Guilherme-mtk@users.noreply.github.com>
2021-03-23 15:49:24 -0300

    adicionado section-2

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-function typewriter(elemento) {
-  const textoArray = elemento.innerText.split('');
-  elemento.innerHTML = '';
-  textoArray.forEach((letra,i)=> {
-    setTimeout(function() {
-        elemento.innerHTML+= letra;
-    }, 75 * i)
-  });
+// function typewriter(elemento) {
+//   const textoArray = elemento.innerText.split('');
+//   elemento.innerHTML = '';
+//   textoArray.forEach((letra,i)=> {
+//     setTimeout(function() {
+//         elemento.innerHTML+= letra;
+//     }, 75 * i)
+//   });
  
-}
+// }
 
 
-const titulo = document.querySelector('h1');
-typewriter(titulo);
+// const titulo = document.querySelector('h1');
+// typewriter(titulo);
 
