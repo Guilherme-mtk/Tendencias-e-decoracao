6800f527fecd672308e9e0772d5615e5e25f91a4
Guilherme Lima <76075799+Guilherme-mtk@users.noreply.github.com>
2021-03-26 16:05:26 -0300

    adicionado efeito scroll no js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+const linkScroll = document.querySelectorAll('.js-menu a[href^="#"]');
 
-// function typewriter(elemento) {
-//   const textoArray = elemento.innerText.split('');
-//   elemento.innerHTML = '';
-//   textoArray.forEach((letra,i)=> {
-//     setTimeout(function() {
-//         elemento.innerHTML+= letra;
-//     }, 75 * i)
-//   });
- 
-// }
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
 
+
+const topp = section.offsetTop;
+window.scrollTo({
+  top: topp,
+  behavior:'smooth',
+});
+}
+
+linkScroll.forEach(link => {
+  link.addEventListener('click', scrollToSection);
+});
 
 
