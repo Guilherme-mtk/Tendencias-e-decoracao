284089082e89b875fe16119850d71a7509208a41
Guilherme Lima <76075799+Guilherme-mtk@users.noreply.github.com>
2021-03-27 09:01:50 -0300

    adicionado section-4

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+
+// function typewriter(elemento) {
+//   const textoArray = elemento.innerText.split('');
+//   elemento.innerHTML = '';
+//   textoArray.forEach((letra,i)=> {
+//     setTimeout(function() {
+//         elemento.innerHTML+= letra;
+//     }, 75 * i)
+//   });
+ 
+// }
+
+function scrolleffect() {
+
+
 const linkScroll = document.querySelectorAll('.js-menu a[href^="#"]');
 
 function scrollToSection(event) {
@@ -17,4 +32,6 @@ linkScroll.forEach(link => {
   link.addEventListener('click', scrollToSection);
 });
 
+}
 
+scrolleffect();
