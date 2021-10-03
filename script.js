b78ee0ef2a098dd870c7f27d07dd001838fdacc9
Guilherme-mtk <guilhermejesuslima32@gmail.com>
2021-10-03 09:54:42 -0300

    modify style

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // function typewriter(elemento) {
 //   const textoArray = elemento.innerText.split('');
 //   elemento.innerHTML = '';
@@ -7,31 +6,27 @@
 //         elemento.innerHTML+= letra;
 //     }, 75 * i)
 //   });
- 
+
 // }
 
 function scrolleffect() {
+  const linkScroll = document.querySelectorAll('.js-menu a[href^="#"]');
 
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
 
-const linkScroll = document.querySelectorAll('.js-menu a[href^="#"]');
+    const topp = section.offsetTop;
+    window.scrollTo({
+      top: topp,
+      behavior: 'smooth',
+    });
+  }
 
-function scrollToSection(event) {
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute('href');
-  const section = document.querySelector(href);
-
-
-const topp = section.offsetTop;
-window.scrollTo({
-  top: topp,
-  behavior:'smooth',
-});
-}
-
-linkScroll.forEach(link => {
-  link.addEventListener('click', scrollToSection);
-});
-
+  linkScroll.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
 }
 
 scrolleffect();
