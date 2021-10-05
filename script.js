bc3f6fbb918c761c7bd71e077acee5ae533b46df
Guilherme-mtk <guilhermejesuslima32@gmail.com>
2021-10-05 17:05:10 -0300

    added slide

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// function typewriter(elemento) {
-//   const textoArray = elemento.innerText.split('');
-//   elemento.innerHTML = '';
-//   textoArray.forEach((letra,i)=> {
-//     setTimeout(function() {
-//         elemento.innerHTML+= letra;
-//     }, 75 * i)
-//   });
-
-// }
-
-function scrolleffect() {
+function scrollSuave() {
   const linkScroll = document.querySelectorAll('.js-menu a[href^="#"]');
 
   function scrollToSection(event) {
@@ -29,4 +18,31 @@ function scrolleffect() {
   });
 }
 
-scrolleffect();
+scrollSuave();
+
+function slide() {
+  let time = 2000,
+    currentImageIndex = 0,
+    images = document.querySelectorAll('#slider img');
+  max = images.length;
+
+  function nextImage() {
+    images[currentImageIndex].classList.remove('selected');
+
+    currentImageIndex++;
+
+    if (currentImageIndex >= max) currentImageIndex = 0;
+
+    images[currentImageIndex].classList.add('selected');
+  }
+
+  function start() {
+    setInterval(() => {
+      nextImage();
+    }, time);
+  }
+
+  window.addEventListener('load', start);
+}
+
+slide();
