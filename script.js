22e941a5e6b1fb5580e2e6e6f8dd1d43ca5aacb1
Guilherme-mtk <guilhermejesuslima32@gmail.com>
2021-10-08 08:27:26 -0300

    added animated scroll

--- script.js
+++ script.js
@@ -91,3 +91,21 @@ function slide() {
 }
 
 slide();
+
+function animascroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  const windowMetade = window.innerHeight * 0.6;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top - windowMetade;
+      if (sectionTop < 0) {
+        section.classList.add('ativo');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', animaScroll);
+}
+
+animascroll();
